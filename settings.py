557447533f7082836c65cff9ab30cccb17746cf9
Igor20099 <megakill123@yandex.ru>
2018-07-20 17:40:07 +0500

    Add bullet.py

--- settings.py
+++ settings.py
@@ -7,5 +7,10 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
-
+        #Скорость корабля
         self.ship_speed_factor = 1.5
+        #Параметры пули
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_color = 60, 60, 60
+        
